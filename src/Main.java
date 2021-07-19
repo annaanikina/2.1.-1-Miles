bbeb939b4a9ac5ceafa60fe07e4a44ba629b3b51
Anna Anikina <anna_anikina@mail.ru>
2021-07-19 21:46:57 +0300

    Names modified

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket_price = 15_675;
-        int one_mile_bonus = 20;
-        int total_mile_bonus = ticket_price / one_mile_bonus;
-        System.out.println(total_mile_bonus);
+        int ticketPrice = 15_675;
+        int oneMileBonus = 20;
+        int totalMileBonus = ticketPrice / oneMileBonus;
+        System.out.println(totalMileBonus);
     }
 }
