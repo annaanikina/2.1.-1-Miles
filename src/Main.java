e820519680de147e06cfa3aa51492bb89e9cc605
Anna Anikina <anna_anikina@mail.ru>
2021-07-19 16:50:41 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket_price = 15_675;
+        int one_mile_bonus = 20;
+        int total_mile_bonus = ticket_price / one_mile_bonus;
+        System.out.println(total_mile_bonus);
+    }
+}
